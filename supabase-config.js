cefead938a7666bff511d660621b622d744be452
JoshGoldy <joshua@keepiton.co.za>
2026-04-11 00:53:32 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
   url: 'https://myldggtkyfdtymaxzspa.supabase.co
 ',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15bGRnZ3RreWZkdHltYXh6c3BhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NTkwMTgsImV4cCI6MjA5MTQzNTAxOH0.jH_51ddXAt1R2YXEcc8r1UJaRq3ydg1aW-B2oByIKvQ',
+  anonKey: 'sb_publishable_5rCL00XZnKY1LBmvnk4hOA_JratAz9n',
 };
