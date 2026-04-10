1dc5028ae4c3274ddc3cbfc82500010e9fb80de7
JoshGoldy <joshua@keepiton.co.za>
2026-04-11 01:55:44 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'https://myldggtkyfdtymaxzspa.supabase.co
-',
+  url: 'https://myldggtkyfdtymaxzspa.supabase.co',
   anonKey: 'sb_publishable_5rCL00XZnKY1LBmvnk4hOA_JratAz9n',
 };
